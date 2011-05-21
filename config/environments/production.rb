574b8b3fcbbdfd593cdab0daf3849e64b97bb06d
Brandur <brandur@mutelight.org>
2011-05-21 11:50:45 -0600

    * adding minify option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Askja::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  BistroCar.minify = true
+
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
